17c962a8fe9776b5e231a4d80f6f3f14cbdb71db
Анастасия Трофимова < >
2016-04-05 04:36:08 +0300

    It works!

--- src/main/java/AreaCheckServlet.java
+++ src/main/java/AreaCheckServlet.java
@@ -14,69 +14,78 @@ public class AreaCheckServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8");
         resp.setCharacterEncoding("UTF-8");
-        String XBuf = req.getParameter("X").replace(',', '.');
-        String YBuf = req.getParameter("Y").replace(',', '.');
-        String RBuf = req.getParameter("R").replace(',', '.');
+        String X_s = req.getParameter("X").replace(',', '.');
+        String Y_s = req.getParameter("Y").replace(',', '.');
+        String R_s = req.getParameter("R").replace(',', '.');
         float X = Float.NaN;
         float Y = Float.NaN;
         float R = Float.NaN;
         try {
-            X = Float.parseFloat(XBuf);
-            Y = Float.parseFloat(YBuf);
-            R = Float.parseFloat(RBuf);
-        } catch (NumberFormatException e) {
+            X = Float.parseFloat(X_s);
+            Y = Float.parseFloat(Y_s);
+            R = Float.parseFloat(R_s);
         }
+        catch (NumberFormatException e) {}
+
         PrintWriter out = resp.getWriter();
         if (!validate(X, Y, R)) {
             out.print("<!DOCTYPE html>\n" +
-                    "<html lang=\"en\">\n" +
-                    "<head>\n" +
-                    "<meta charset=\"UTF-8\">\n" +
-                    "<title>Labwork №7</title>\n" +
-                    "</head>\n" +
-                    "<body>\n" +
-                    "<p>Input error!</p>\n" +
-                    "<p>Y must be [-5;5]" + "</p>\n" +
-                    "<p  align=\"center\"><a href=\"/Lab7/main\">Go back</a></p>" +
-                    "</body>\n" +
-                    "</html>");
+                        "<html lang=\"en\">\n" +
+                            "<head>\n" +
+                                "<meta charset=\"UTF-8\">\n" +
+                                "<title>Result</title>\n" +
+                            "</head>\n" +
+                            "<body align=\"center\">\n" +
+                                "<p>Input error!</p>\n" +
+                                "<p>\"Y\" must be [-5;5]. Your \"Y\" = " + Y_s + "</p>\n" +
+                                "<p  align=\"center\"><a href=\"/Lab7/main\">Go back</a></p>" +
+                            "</body>\n" +
+                        "</html>");
             out.close();
             return;
         }
         out.print("<!DOCTYPE html>\n" +
                 "<html lang=\"en\">\n" +
-                "<head>\n" +
-                "    <meta charset=\"UTF-8\">\n" +
-                "    <title>Labwork №7</title>\n" +
-                "</head>\n" +
-                "<body>\n" +
-                "<table align='center'>\n" +
-                "<tr>\n" +
-                "<td>X: </td>\n" +
-                "<td>" + X + "</td>\n" +
-                "</tr>\n" +
-                "<tr>\n" +
-                "<td>Y: </td>\n" +
-                "<td>" + Y + "</td>\n" +
-                "</tr>\n" +
-                "<tr>\n" +
-                "<td>R: </td>\n" +
-                "<td>" + R + "</td>\n" +
-                " <tr>\n" +
-                "<td>In area: </td>\n" +
-                "<td>" + (inFigure(X, Y, R) ? "Yes" : "No") + "</td>\n" +
-                "</tr>\n" +
-                "</table>\n" +
+                    "<head>\n" +
+                        "<meta charset=\"UTF-8\">\n" +
+                        "<title>Result</title>\n" +
+                        "<style>\n" +
+                            ".tbl1 tr td {\n" +
+                                "border: 1px solid black;\n" +
+                                "padding: 10px;\n" +
+                                "text-align: center;\n" +
+                                "border-collapse: separate;\n" +
+                            "}\n" +
+                        "</style>\n" +
+                    "</head>\n" +
+                    "<body>\n" +
+                        "<table class=\"tbl1\" align= \"center\">\n" +
+                            "<tr>\n" +
+                                "<td>X: </td>\n" +
+                                "<td>" + X + "</td>\n" +
+                            "</tr>\n" +
+                            "<tr>\n" +
+                                "<td>Y: </td>\n" +
+                                "<td>" + Y + "</td>\n" +
+                            "</tr>\n" +
+                            "<tr>\n" +
+                                "<td>R: </td>\n" +
+                                "<td>" + R + "</td>\n" +
+                                "<tr>\n" +
+                                "<td>In area: </td>\n" +
+                                "<td>" + (inArea(X, Y, R) ? "Yes" : "No") + "</td>\n" +
+                            "</tr>\n" +
+                        "</table>\n" +
                 "<p  align=\"center\"><a href=\"/Lab7/main\">Go back</a></p>" +
                 "</body>\n" +
                 "</html>");
         out.close();
     }
 
-    boolean inFigure(float X, float Y, float R) {
-        return (Y >= 0 && Y <= R && X <= 0 && X >= R / 2) &&
-                (Y >= -R / 2 && Y <= 0 && X >= -R / 2 && X <= 0 && (X * X + Y * Y) >= R * R) &&
-                (Y >= -R / 2 && Y <= 0 && X >= 0 / 2 && X <= R && Y >= X / 2 - R / 2);
+    boolean inArea(float X, float Y, float R) {
+        return (((X <= 0) && (X >= -R) && (Y <= 0) && (Y >= -R / 2)) ||
+                ((X >= 0) && (X <= R) && (Y >= -R) && (Y <= 0) && (X * X + Y * Y) <= R * R / 4) ||
+                ((X >= 0) && (X <= R) && (Y >= 0) && X <= R && (Y <= -X + R)));
     }
 
     boolean validate(float X, float Y, float R) {
--- src/main/java/WebFormServlet.java
+++ src/main/java/WebFormServlet.java
@@ -17,117 +17,111 @@ import java.io.PrintWriter;
             out.print("<!DOCTYPE html>\n" +
             "<html lang = \"en\">\n" +
             "<head>\n" +
-                    "<meta charset=\"UTF-8\"> \n" +
-                    "<title>Labwork №7</title>\n" +
-                    "<style>\n" +
-                    ".content{\n" +
-                    "margin-top: 50px;\n" +
+                "<meta charset=\"UTF-8\"> \n" +
+                "<title>Labwork №7</title>\n" +
+                "<style>\n" +
+                    "#head {\n" +
+                        "font: 150% serif;\n" +
+                        "background: #F1AD2E;\n" +
+                        "text-align: center;\n" +
+                    "}\n" +
+                    "#head:hover {\n" +
+                        "color: #B83129;\n" +
+                        "}\n" +
+                    ".radius {\n" +
+                        "border: 1px solid black;\n" +
+                        "padding: 15px;\n" +
+                        "margin-bottom: 10px;\n" +
                     "}\n" +
                     ".inputs{\n" +
-                    "width: 50%;\n" +
-                    "float: left;\n" +
-                    "padding-top: 50px;\n" +
-                    "}n" +
+                        "width: 50%;\n" +
+                        "float: left;\n" +
+                        "padding-top: 50px;\n" +
+                    "}\n" +
                     ".wrapper{\n" +
-                    "margin: auto;\n" +
-                    "width: 980px;\n" +
+                        "margin: auto;\n" +
+                        "width: 980px;\n" +
                     "}\n" +
                     ".area{\n" +
-                    "width: 50%;\n" +
-                    "float: right;\n" +
-                    "padding: auto;\n" +
-                    "text-align: center;\n" +
+                        "width: 50%;\n" +
+                        "float: right;\n" +
+                        "padding: auto;\n" +
+                        "text-align: center;\n" +
                     "}\n" +
                     ".area::before{\n" +
-                    "content: \"Область:\";\n" +
-                    "display: block;\n" +
-                    "}\n" +
-                    "header>p{\n" +
-                    "font-family: sans-serif;\n" +
-                    "width: 300px;\n" +
-                    "padding: 5px;\n" +
-                    "text-align: center;\n" +
-                    "margin: auto;\n" +
-                    "color: #004dff;\n" +
-
+                        "content: \"Graph:\";\n" +
+                        "display: block;\n" +
                     "}\n" +
                     "form{\n" +
-                    "font-family: serif;\n" +
+                        "font-family: serif;\n" +
                     "}\n" +
                     "form>p>input,button,select{\n" +
-                    "margin-left: 10px;\n" +
+                        "margin-left: 10px;\n" +
                     "}\n" +
                     "form>p>button{\n" +
-                    "width: 30px;\n" +
-                    "height: 30px;\n" +
+                        "width: 30px;\n" +
+                        "height: 30px;\n" +
                     "}\n" +
-                    ".radius {\n" +
-                    "border: 1px solid black;\n" +
-                    "padding: 15px;\n" +
-                    "margin-bottom: 10px;\n" +
-                    "}\n" +
-                    "#head {\n" +
-                    "font: 150% serif;\n" +
-                    "background: #F1AD2E;\n" +
-                    "}\n" +
-                    "</style>\n"+
+
+                "</style>\n"+
                     "</head>\n" +
-                    "<body>\n" +
-                    "<div class=\"wrapper\">\n" +
-                    "<header id=\"head\" align= \"center\" style=\"border-radius: 65px 0px\" class=\"radius\">\n" +
-                    "<font>\n" +
-                    "Трофимова Анастасия Михайловна<Br>\n" +
-                    "Группа P3202<Br>\n" +
-                    "Вариант 218\n" +
-                    "</font>\n" +
-                    "</header>\n" +
-                    "<div class=\"content\">\n" +
-                    "<div class=\"inputs\">\n" +
-                    "<form method=\"GET\" id=\"forma\" action=\"/Lab7/main\">\n" +
-                    "<p>R:\n" +
-                    "<input type=\"radio\" name=\"R\" value=\"1\" required>1\n " +
-                    "<input type=\"radio\" name=\"R\" value=\"2\" required>2\n " +
-                    "<input type=\"radio\" name=\"R\" value=\"3\" required>3\n " +
-                    "<input type=\"radio\" name=\"R\" value=\"3\" required>4\n " +
-                    "<input type=\"radio\" name=\"R\" value=\"5\" required>5\n " +
-                    "</p>\n" +
-                    "<p>Y:\n" +
-                    "<input type=\"text\" id=\"textY\" name=\"Y\" placeholder=\"-5..5\" onchange=\"validateForm()\" onkeydown=\"javascript:if(13==event.keyCode){return false;}\"  required>\n" +
-            "</p>\n" +
-            "<p>X:\n" +
-            "<input class=\"txt1\" id=\"idX1\" type=\"submit\" name=\"X\" value=\"1\">\n" +
-            "<input class=\"txt1\" id=\"idX2\" type=\"submit\" name=\"X\" value=\"2\">\n" +
-            "<input class=\"txt1\" id=\"idX3\" type=\"submit\" name=\"X\" value=\"3\">\n" +
-            "<input class=\"txt1\" id=\"idX4\" type=\"submit\" name=\"X\" value=\"4\">\n" +
-            "<input class=\"txt1\" id=\"idX5\" type=\"submit\" name=\"X\" value=\"5\">\n" +
-            "</p>\n" +
-            "</form>\n" +
-            "</div>\n" +
-            "<div class=\"area\">\n" +
-            "</div>\n" +
-            "</div>\n" +
-            "</div>\n" +
-            "</body>\n" +
+                        "<body>\n" +
+                            "<div class=\"wrapper\">\n" +
+                                "<header id=\"head\" align=\"center\" style=\"border-radius: 65px 0px\" class=\"radius\">\n" +
+                                    "<font>\n" +
+                                        "Трофимова Анастасия Михайловна<Br>\n" +
+                                        "Группа P3202<Br>\n" +
+                                        "Вариант 218\n" +
+                                    "</font>\n" +
+                                "</header>\n" +
+                                "<div class=\"content\">\n" +
+                                "<div class=\"inputs\">\n" +
+                                    "<form method=\"GET\" id=\"forma\" action=\"/Lab7/main\">\n" +
+                                        "<p>R:\n" +
+                                            "<input type=\"radio\" name=\"R\" value=\"1\" required>1\n " +
+                                            "<input type=\"radio\" name=\"R\" value=\"2\" required>2\n " +
+                                            "<input type=\"radio\" name=\"R\" value=\"3\" required>3\n " +
+                                            "<input type=\"radio\" name=\"R\" value=\"3\" required>4\n " +
+                                            "<input type=\"radio\" name=\"R\" value=\"5\" required>5\n " +
+                                        "</p>\n" +
+                                        "<p>Y:\n" +
+                                            "<input type=\"text\" id=\"textY\" name=\"Y\" placeholder=\"-5..5\" onchange=\"validateForm()\" onkeydown=\"javascript:if(13==event.keyCode){return false;}\"  required>\n" +
+                                        "</p>\n" +
+                                        "<p>X:\n" +
+                                            "<input class=\"dotX\" id=\"idX1\" type=\"submit\" name=\"X\" value=\"1\">\n" +
+                                            "<input class=\"dotX\" id=\"idX2\" type=\"submit\" name=\"X\" value=\"2\">\n" +
+                                            "<input class=\"dotX\" id=\"idX3\" type=\"submit\" name=\"X\" value=\"3\">\n" +
+                                            "<input class=\"dotX\" id=\"idX4\" type=\"submit\" name=\"X\" value=\"4\">\n" +
+                                            "<input class=\"dotX\" id=\"idX5\" type=\"submit\" name=\"X\" value=\"5\">\n" +
+                                        "</p>\n" +
+                                    "</form>\n" +
+                                "</div>\n" +
+                                "<div class=\"area\">\n" +
+                                "<img src=\"http://savepic.ru/9271508.png\">\n" +
+                                "</div>\n" +
+                            "</div>\n" +
+                            "</div>\n" +
+                        "</body>\n" +
             "</html>\n" +
             "<script>\n" +
-            "function validateForm(){\n" +
-            "var _y = document.getElementById(\"textY\").value;\n" +
-            "_y = _y.replace(\",\",\".\");\n" +
-            "var _yvalid = !((_y == \"\") || !(!isNaN(parseFloat(_y)) && isFinite(_y)) || (_y > 5) || (_y < -5));\n" +
-            "if (_yvalid) {\n" +
-                    "document.getElementById(\"idX1\").disabled=false;\n" +
-                    "document.getElementById(\"idX2\").disabled=false;\n" +
-                    "document.getElementById(\"idX3\").disabled=false;\n" +
-                    "document.getElementById(\"idX4\").disabled=false;\n" +
-                    "document.getElementById(\"idX5\").disabled=false;\n" +
-            "}else{\n" +
-                    "document.getElementById(\"idX1\").disabled=true;\n" +
-                    "document.getElementById(\"idX2\").disabled=true;\n" +
-                    "document.getElementById(\"idX3\").disabled=true;\n" +
-                    "document.getElementById(\"idX4\").disabled=true;\n" +
-                    "document.getElementById(\"idX5\").disabled=true;\n" +
-            "}\n" +
-            "}\n" +
+                "function validateForm(){\n" +
+                    "var _y = document.getElementById(\"textY\").value;\n" +
+                    "_y = _y.replace(\",\",\".\");\n" +
+                    "var _yvalid = !((_y == \"\") || !(!isNaN(parseFloat(_y)) && isFinite(_y)) || (_y > 5) || (_y < -5));\n" +
+                    "if (_yvalid) {\n" +
+                        "document.getElementById(\"idX1\").disabled=false;\n" +
+                        "document.getElementById(\"idX2\").disabled=false;\n" +
+                        "document.getElementById(\"idX3\").disabled=false;\n" +
+                        "document.getElementById(\"idX4\").disabled=false;\n" +
+                        "document.getElementById(\"idX5\").disabled=false;\n" +
+                    "}else{\n" +
+                        "document.getElementById(\"idX1\").disabled=true;\n" +
+                        "document.getElementById(\"idX2\").disabled=true;\n" +
+                        "document.getElementById(\"idX3\").disabled=true;\n" +
+                        "document.getElementById(\"idX4\").disabled=true;\n" +
+                        "document.getElementById(\"idX5\").disabled=true;\n" +
+                    "}\n" +
+                "}\n" +
             "</script>\n");
             out.close();
         }
